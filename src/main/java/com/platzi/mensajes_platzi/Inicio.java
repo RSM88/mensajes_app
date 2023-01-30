740a694ab121330afe7952d07eb9839f9f812e88
Robert Siles <88rmr88@gmail.com>
2023-01-30 12:01:25 -0600

    Inicio de proyecto

---
+++ src/main/java/com/platzi/mensajes_platzi/Inicio.java
@@ -0,0 +1,18 @@
+package com.platzi.mensajes_platzi;
+
+import java.sql.Connection;
+
+public class Inicio {
+	
+	public static void main(String[] args) {
+		
+		Conexion conexion = new Conexion();
+		
+		try(Connection cnx = conexion.get_connection()){
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
